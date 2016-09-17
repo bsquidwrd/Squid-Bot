96291872406aa6c91987ab4f8b928c649b7e8411
bsquidwrd <git@bsquid.io>
2016-09-17 00:42:46 -0700

    maybe if I type it right, it will work

--- web/urls.py
+++ web/urls.py
@@ -17,12 +17,13 @@ from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.views import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
 if not settings.DEBUG:
-    urlpattherns.append(
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
+    urlpatterns.append(
+        url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT})
     )
