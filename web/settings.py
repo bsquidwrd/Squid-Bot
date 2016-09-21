7d88d13cb7a55830e8dca91b745e22934cc3a2b8
bsquidwrd <git@bsquid.io>
2016-09-21 23:30:51 +0000

    Added to requirements.txt and made a change to web/settings.py

--- web/settings.py
+++ web/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SQUID_BOT_DJANGO_SECRET', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-debug_mode = os.getenv('SQUID_BOT_DEBUG_MODE', True)
+debug_mode = os.getenv('SQUID_BOT_DEBUG_MODE', 'true')
 if debug_mode.lower() == 'false':
     DEBUG = False
 else:
