55677c80eca9871173122fc9fc22122a1ca062b6
bsquidwrd <git@bsquid.io>
2016-10-29 23:25:44 -0700

    Added a real task to delete channels after their expiration has ended

--- cogs/gaming_tasks.py
+++ cogs/gaming_tasks.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from .utils import checks
 import asyncio
 import discord
+import datetime
 
 import web.wsgi
 from django.utils import timezone
@@ -21,7 +22,17 @@ class GamingTasks:
     def run_tasks(bot):
         while True:
             yield from asyncio.sleep(30)
-
+            channels = Channel.objects.filter(private=Faalse, expire_date__lte=datetime.datetime.now())
+            for channel in channels:
+                try:
+                    c = yield from self.bot.get_channel(channel.channel_id)
+                    yield from self.bot.delete_channel(c)
+                except Exception as e:
+                    # Channel no longer exists on server
+                    pass
+                finally:
+                    channel.deleted = True
+                    channel.save()
 
 def setup(bot):
     loop = asyncio.get_event_loop()
