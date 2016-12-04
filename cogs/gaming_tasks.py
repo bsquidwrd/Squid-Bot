36e33f3b63efdd0ae003ecf5bf389895d505237e
bsquidwrd <git@bsquid.io>
2016-12-03 22:48:07 -0800

    Less emails and fixing logify_exception_info

--- cogs/gaming_tasks.py
+++ cogs/gaming_tasks.py
@@ -25,7 +25,7 @@ class GamingTasks:
             try:
                 yield from asyncio.sleep(5)
 
-                log_item = Log(message="This should not be saved, ever, unless something went wrong", email=True)
+                log_item = Log(message="This should not be saved, ever, unless something went wrong")
 
                 channels = Channel.objects.filter(private=False, game_channel=True, expire_date__lte=timezone.now(), deleted=False)
                 if channels.count() >= 1:
@@ -41,10 +41,10 @@ class GamingTasks:
                                     yield from bot.delete_channel(c)
                                     log_item.message += '- Success'
                                 except Exception as e:
-                                    log_item.message += '- Failure\n\n{}'.format(logify_exception_info(e))
+                                    log_item.message += '- Failure\n\n{}{}\n'.format(logify_exception_info(), e)
                         except Exception as e:
                             # Channel no longer exists on server
-                            log_item.message += '- Failure\n\n{}'.format(logify_exception_info(e))
+                            log_item.message += '- Failure\n\n{}{}\n'.format(logify_exception_info(), e)
                         finally:
                             channel.deleted = True
                             channel.save()
@@ -55,29 +55,30 @@ class GamingTasks:
                     # No channels found to be deleted
                     pass
 
-                log_item = Log(message="This should not be saved, ever, unless something went wrong", email=True)
+                log_item = Log(message="This should not be saved, ever, unless something went wrong")
 
                 for server in Server.objects.all():
                     # This will be to populate Channels and Users
+                    s = bot.get_server(server.server_id)
                     log_item = Log.objects.create(message="Starting population of channel users for server {}\n\n".format(server))
                     try:
                         channels = Channel.objects.filter(server=server, deleted=False)
-                        log_item.message += "- Found channels:\n{}\n\n".format(logify_object(channels))
+                        log_item.message += "Found channels:\n{}\n\n".format(logify_object(channels))
                         for channel in channels:
                             try:
                                 log_item.message += "Running for channel {}\n".format(channel)
-                                c = self.bot.get_channel(channel.channel_id)
+                                c = bot.get_channel(channel.channel_id)
                                 if c is None:
                                     log_item.message += "- Channel is not found, marking as deleted\n"
                                     c.deleted = True
                                     c.save()
                                     continue
                                 user_ids = [su.user.pk for su in ServerUser.objects.filter(server=server)]
-                                log_item.message += "  - Users found for this channel: {}\n".format(user_ids)
+                                log_item.message += "  - Users found for this server, qualified for this channel: {}\n".format(user_ids)
                                 for user in DiscordUser.objects.filter(pk__in=user_ids):
                                     try:
                                         log_item.message += "    - Working on user {}\n".format(user)
-                                        perms = c.permissions_for(discord.User(id=user.user_id))
+                                        perms = c.overwrites_for(discord.User(id=user.user_id))
                                         log_item.message += "      - Perms: {}\n".format(perms)
                                         cu, cu_created = ChannelUser.objects.get_or_create(channel=channel, user=user)
                                         log_item.message += "        - CU, Created: {},{}\n".format(cu, cu_created)
@@ -86,19 +87,19 @@ class GamingTasks:
                                                 log_item.message += "      - Read Messages: {} - Send Messages: {}\n".format(perms.read_messages, perms.send_messages)
                                                 cu.delete()
                                     except Exception as e:
-                                        log_item.message += "- Failed: {}\n".format(logify_exception_info(e))
+                                        log_item.message += "- Failed: {}\n{}\n".format(logify_exception_info(), e)
                                     log_item.message += "\n"
                             except Exception as e:
-                                log_item.message += "- Failed: {}\n".format(logify_exception_info(e))
+                                log_item.message += "- Failed:\n{}\n{}\n".format(logify_exception_info(), e)
                             finally:
-                                log_item.message += "- Finished channel processing\n"
+                                log_item.message += "- Finished channel processing\n{}{}\n".format(logify_exception_info(), e)
                         log_item.message += "\n"
                     except Exception as e:
-                        log_item.message += "- Failed: {}\n".format(logify_exception_info(e))
+                        log_item.message += "- Failed: {}\n{}\n".format(logify_exception_info(), e)
                     finally:
                         log_item.save()
 
-                log_item = Log(message="This should not be saved, ever, unless something went wrong", email=True)
+                log_item = Log(message="This should not be saved, ever, unless something went wrong")
 
                 tasks = Task.objects.filter(cancelled=False, completed=False, expire_date__lte=timezone.now())
                 if tasks.count() >= 1:
@@ -112,18 +113,18 @@ class GamingTasks:
                                 channel = Channel.objects.get(pk=task.channel.pk)
                                 log_item.message += 'Game: {}\nChannel: {}\nUser: {}\nTask PK: {}\n\n'.format(user, game, channel, task.pk)
                             except Game.DoesNotExist as e:
-                                log_item.message += '- Error finding game for task.\n{}'.format(logify_exception_info(e))
+                                log_item.message += '- Error finding game for task.\n{}{}\n'.format(logify_exception_info(), e)
                             except Channel.DoesNotExist as e:
-                                log_item.message += '- Error finding channel for game {}\n{}'.format(game, logify_exception_info(e))
+                                log_item.message += '- Error finding channel for game {}\n{}{}\n'.format(game, logify_exception_info(), e)
                             except Exception as e:
-                                log_item.message += '- Unknown error happened\n{}'.format(logify_exception_info(e))
+                                log_item.message += '- Unknown error happened\n{}{}\n'.format(logify_exception_info(), e)
                         else:
                             # Not really a task for some reason
                             log_item.message += '- I don\'t recognize the task type \'{}\'...'.format()
                         log_item.message += '- Finished processing task\n'
                         log_item.save()
             except Exception as e:
-                log_item = Log.objects.create(message="- Error running task:\n{}".format(logify_exception_info(e)))
+                log_item = Log.objects.create(message="- Error running task:\n{}{}\n".format(logify_exception_info(), e))
             finally:
                 log_item.save()
 
