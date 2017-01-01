231fe3956df2bf62796cd7ca7ed51da22fb5b90d
bsquidwrd <git@bsquid.io>
2017-01-01 01:47:06 -0800

    Decided against not deleting messages

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -170,7 +170,6 @@ class Quotes:
             try:
                 quote = Quote.objects.create(timestamp=timezone.now(), user=quote_user, added_by=user, server=server, message=content)
                 await self.bot.say("{0}, Your quote was create successfully!\nThe Quote ID is `{1}`\nYou can use this to reference it in the future by typing `?quote get {1}`".format(ctx.message.author.mention, quote.quote_id), delete_after=30)
-                await self.bot.delete_message(ctx.message)
             except Exception as e:
                 log_item = Log.objects.create(message="{}\nError creating Quote\n{}\nquote_user: {}\nuser: {}\nserver: {}\nmessage: {}\nmentions: {}".format(logify_exception_info(), e, quote_user, user, server, message, mentions))
                 await self.bot.say("{}, There was an error when trying to create your Quote. Please contact my Owner with the following code: `{}`".format(ctx.message.author.mention, log_item.message_token), delete_after=30)
@@ -219,6 +218,7 @@ class Quotes:
 
         else:
             await self.bot.say("{}: I didn't quite understand your command, please run `?help quote` to learn how to use this command.".format(ctx.message.author.mention), delete_after=30)
+        await self.bot.delete_message(ctx.message)
     # End Commands
 
     # Errors
